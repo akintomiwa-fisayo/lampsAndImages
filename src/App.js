04c7f02805b9f422ea6ebd0b6d4404d02edc91b3
akintomiwa fisayo <akintomiwa.fisayo@gmail.com>
2020-03-24 18:46:02 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -17,9 +17,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <MainContent />
-        <Footer />
+        <Router basename={`${process.env.PUBLIC_URL}/`}>
+          <Header />
+          <MainContent />
+          <Footer />
+        </Router>
       </div>
     );
   }
